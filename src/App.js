f2ed9255f72d89ccc62281527ae357ef8ea8a886
Jez Templeton <j.templeton@bom.gov.au>
2017-07-30 17:21:29 +1000

    Add search, and update and cleanup other bits.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import Forecast from './components/Forecast/Forecast'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div style={{
+        display: 'flex',
+        flex: '1 1 auto',
+        flexFlow: 'row nowrap',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}>
         <Forecast />
       </div>
     );
--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import ContentEditable from 'react-contenteditable'
+import debounce  from 'lodash.debounce'
 import { url, apiKey } from '../../credentials.json'
 
 export default class Forecast extends Component {
@@ -8,10 +9,10 @@ export default class Forecast extends Component {
     super()
     this.state = {
       forecast: [
-        {value: 'something'},
+        {value: 'sunny'},
       ],
       location: {
-        name: 'hobart'
+        name: 'philladelphia'
       }
     }
   }
@@ -36,22 +37,38 @@ export default class Forecast extends Component {
     return this.getRequest(`search/v1/locations/?q=${this.state.location.name}`)
   }
 
-  async getForecast() {
+  getForecast() {
     if (typeof this.state.location.geohash !== undefined) {
       return this.getRequest(`forecasts/v1/grid/three-hourly/${this.state.location.geohash}/icons`)
     }
   }
 
-  componentDidMount() {
+  updateForecast() {
     this.getLocation().then(
       (location) => {
-        this.setState({location: {...location[0].attributes }})
-        this.getForecast().then(
-          (forecasts) => this.setState({forecast: {...forecasts.attributes.icon_descriptor.forecast_data }})
-        )
+        if(typeof location !== 'undefined' && location.length > 0){
+          this.setState({location: {...location[0].attributes }})
+          this.getForecast().then(
+            (forecasts) => this.setState({forecast: {...forecasts.attributes.icon_descriptor.forecast_data }})
+          )
+        }
       }
     )
   }
+
+  componentDidMount() {
+    this.updateForecast()
+    this.editableChange = debounce(this.editableChange, 300)
+  }
+
+  editableChange(input) {
+    const filtered = input.replace(/\s/g, ' ').replace(/\W+/g, ' ');
+    console.log(filtered)
+    if(filtered.length >= 3) {
+      this.setState({location: { name: filtered}}, this.updateForecast)
+    }
+  }
+
 
   phillycast(precis){
     let phillycast = precis
@@ -67,8 +84,14 @@ export default class Forecast extends Component {
     const weather = this.phillycast(this.state.forecast[0].value)
     const place = this.state.location.name
 
+    const debouncedEditableChange = debounce(this.editableChange.bind(), 300)
     return (
-      <section>
+      <section style={{
+        display: 'flex',
+        flex: '1 1 auto',
+        flexFlow: 'column nowrap',
+        textAlign: 'center',
+      }}>
         <h1 style={{
           fontSize: '72px',
           fontWeight: '700',
@@ -80,7 +103,9 @@ export default class Forecast extends Component {
           <div style={{ fontSize: '108px'}}>always</div>
           <div><span style={{ display: 'inline-block', fontSize: '128px', transform: 'rotateZ(-2deg)'}}>{weather}</span>
           <span> in</span></div>
-          <div style={{ position: 'relative', top: '-30px'}}><span spellCheck='false' contentEditable>{place}</span>!</div>
+          <div style={{ position: 'relative', top: '-30px'}}>
+            <ContentEditable html={place} spellCheck={false} onChange={(event) => this.editableChange(event.target.value)} />
+          </div>
         </h1>
       </section>
     )
